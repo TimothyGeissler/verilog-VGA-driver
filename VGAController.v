b8e50877dbfadd44a15bbdd08889844f0e1195f7
Tim G <tdg24@duke.edu>
2023-03-02 15:58:02 -0500

    VGA driver w/ moving square & image display

---
+++ VGAController.v
@@ -0,0 +1,138 @@
+`timescale 1 ns/ 100 ps
+module VGAController(     
+	input clk, 			// 100 MHz System Clock
+	input reset, 		// Reset Signal
+	input BTNU,			// Up button
+	input BTND,			// Down button
+	input BTNL,			// Left button
+	input BTNR,			// Right button
+	output hSync, 		// H Sync Signal
+	output vSync, 		// Veritcal Sync Signal
+	output[3:0] VGA_R,  // Red Signal Bits
+	output[3:0] VGA_G,  // Green Signal Bits
+	output[3:0] VGA_B,  // Blue Signal Bits
+	inout ps2_clk,
+	inout ps2_data);
+	
+	// Lab Memory Files Location
+	localparam FILES_PATH = "~/Documents/Verilog/Lab5/Lab5/";
+
+	// Clock divider 100 MHz -> 25 MHz
+	wire clk25; // 25MHz clock
+
+	reg[1:0] pixCounter = 0;      // Pixel counter to divide the clock
+    assign clk25 = pixCounter[1]; // Set the clock high whenever the second bit (2) is high
+	always @(posedge clk) begin
+		pixCounter <= pixCounter + 1; // Since the reg is only 3 bits, it will reset every 8 cycles
+	end
+
+	// VGA Timing Generation for a Standard VGA Screen
+	localparam 
+		VIDEO_WIDTH = 640,  // Standard VGA Width
+		VIDEO_HEIGHT = 480; // Standard VGA Height
+
+	wire active, screenEnd;
+	wire[9:0] x;
+	wire[8:0] y;
+	
+	VGATimingGenerator #(
+		.HEIGHT(VIDEO_HEIGHT), // Use the standard VGA Values
+		.WIDTH(VIDEO_WIDTH))
+	Display( 
+		.clk25(clk25),  	   // 25MHz Pixel Clock
+		.reset(reset),		   // Reset Signal
+		.screenEnd(screenEnd), // High for one cycle when between two frames
+		.active(active),	   // High when drawing pixels
+		.hSync(hSync),  	   // Set Generated H Signal
+		.vSync(vSync),		   // Set Generated V Signal
+		.x(x), 				   // X Coordinate (from left)
+		.y(y)); 			   // Y Coordinate (from top)	
+
+	// Image Data to Map Pixel Location to Color Address
+	localparam 
+		PIXEL_COUNT = VIDEO_WIDTH*VIDEO_HEIGHT, 	             // Number of pixels on the screen
+		PIXEL_ADDRESS_WIDTH = $clog2(PIXEL_COUNT) + 1,           // Use built in log2 command
+		BITS_PER_COLOR = 12, 	  								 // Nexys A7 uses 12 bits/color
+		PALETTE_COLOR_COUNT = 256, 								 // Number of Colors available
+		PALETTE_ADDRESS_WIDTH = $clog2(PALETTE_COLOR_COUNT) + 1; // Use built in log2 Command
+
+	wire[PIXEL_ADDRESS_WIDTH-1:0] imgAddress;  	 // Image address for the image data
+	wire[PALETTE_ADDRESS_WIDTH-1:0] colorAddr; 	 // Color address for the color palette
+	assign imgAddress = x + 640*y;				 // Address calculated coordinate
+
+	RAM #(		
+		.DEPTH(PIXEL_COUNT), 				     // Set RAM depth to contain every pixel
+		.DATA_WIDTH(PALETTE_ADDRESS_WIDTH),      // Set data width according to the color palette
+		.ADDRESS_WIDTH(PIXEL_ADDRESS_WIDTH),     // Set address with according to the pixel count
+		.MEMFILE({FILES_PATH, "bright_colors.mem"})) // Memory initialization
+	ImageData(
+		.clk(clk), 						 // Falling edge of the 100 MHz clk
+		.addr(imgAddress),					 // Image data address
+		.dataOut(colorAddr),				 // Color palette address
+		.wEn(1'b0)); 						 // We're always reading
+
+	// Color Palette to Map Color Address to 12-Bit Color
+	wire[BITS_PER_COLOR-1:0] colorData, rendered; // 12-bit color data at current pixel
+
+		//Draw square
+		reg [9:0] sqX = 240;
+		reg [8:0] sqY = 200; // Store top left corner
+		wire xMatch, yMatch, inBounds; //boolean wires
+
+		assign xMatch = ((0 < x - sqX) && (x - sqX < 75)); // = 1 if in bounds, 0 else
+		assign yMatch = ((0 < y - sqY) && (y - sqY < 75));
+		assign inBounds = yMatch & xMatch;
+
+
+		// if in range then display square color, else display colorData
+		assign rendered = inBounds ? 12'd0 : colorData;
+
+
+		// Update x & y coords of square if button pressed
+		always @(posedge screenEnd) begin
+			if (BTND == 1'b1) begin
+				sqY = sqY + 1;
+			end else if (BTNU == 1'b1) begin
+				sqY = sqY - 1;
+			end else if (BTNR == 1'b1) begin
+				sqX = sqX + 1;
+			end else if (BTNL == 1'b1) begin
+				sqX = sqX - 1;
+			end
+		end
+		/*
+		always @(BTND) begin
+			sqY = sqY + 1;
+		end
+
+		always @(BTNU) begin
+			sqY = sqY - 1;
+		end
+
+		always @(BTNR) begin
+			sqX = sqX + 1;
+		end
+
+		always @(BTNL) begin
+			sqX = sqX - 1;
+		end*/
+
+	RAM #(
+		.DEPTH(PALETTE_COLOR_COUNT), 		       // Set depth to contain every color		
+		.DATA_WIDTH(BITS_PER_COLOR), 		       // Set data width according to the bits per color
+		.ADDRESS_WIDTH(PALETTE_ADDRESS_WIDTH),     // Set address width according to the color count
+		.MEMFILE({FILES_PATH, "colors.mem"}))  // Memory initialization
+	ColorPalette(
+		.clk(clk), 							   	   // Rising edge of the 100 MHz clk
+		.addr(colorAddr),					       // Address from the ImageData RAM
+		.dataOut(colorData),				       // Color at current pixel
+		.wEn(1'b0)); 						       // We're always reading
+	
+
+	// Assign to output color from register if active
+	wire[BITS_PER_COLOR-1:0] colorOut; 			  // Output color 
+	assign colorOut = active ? rendered : 12'd0; // When not active, output black
+
+	// Quickly assign the output colors to their channels using concatenation
+	assign {VGA_R, VGA_G, VGA_B} = colorOut;
+endmodule
